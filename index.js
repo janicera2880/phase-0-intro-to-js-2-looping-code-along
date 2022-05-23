d6bf2a012e5656f901bba6d5fcdbd84c54639bef
Janice Alecha <janiceralecha@gmail.com>
2022-05-22 21:35:27 -0500

    commit tree 458fa8cc8ddff231d733acc339b6a4b0d70bd7de

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+const messages = [];
+function writeCards(names, holiday) {
+  {
+  
+  for(let count = 0; count < names.length; count++)
+      {
+      messages.push(`Thank you, ${names[count]}, for the wonderful ${holiday} gift!`);
+      }
+  }
+return messages;
+}
+
+function countDown(enteredNumber) {
+  {
+
+while(enteredNumber >= 0) 
+{
+console.log(enteredNumber);
+enteredNumber--;
+      }
+  }
+}
